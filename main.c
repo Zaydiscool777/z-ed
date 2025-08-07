c6ef650c97c197a42dcf3201584689e6104eb7b7
zaydm <Zaydiscool777@gmail.com>
2025-08-07 03:02:44 +0000

    something... added file handling?

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
+// #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main(){
+    return 0;
+}
+
+char filename[256] = "a";
+FILE* buffer(int form){
+    if(form){
+        FILE* fp = fopen(filename, "w");
+        return fp;
+    } else {
+        FILE* fp = fopen(filename, "r");
+        if(fp == NULL){
+            fp = fopen(filename, "w");
+            return fp;
+            // TODO: POSIX requires errno here
+        } else {
+            return fp;
+        }
+    }
 }
